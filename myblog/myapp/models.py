f1c036d70c42e376fd90752b21e783cd92dd67f4
iuliagheju <162321949+iuliagheju@users.noreply.github.com>
2024-06-17 20:01:05 +0300

    commit

--- myblog/myapp/models.py
+++ myblog/myapp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     post_date = models.DateField()
-    image = models.ImageField(upload_to='static/')
+    image = models.ImageField(upload_to='static/', blank=True, null=True)
     paragraph = models.TextField(max_length=2000)
     more_details_for_the_dedicated_page = models.TextField(max_length=1000,
 default=' ')
-    def __str__(self):
-        return self.title
+    
+  
--- myblog/myapp/urls.py
+++ myblog/myapp/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='index'),   
+    path('', views.home, name='home'),   
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('chatbot/', views.chatbot, name='chatbot'),
     path('form/', views.form, name='form'),
     path('gallery/', views.gallery, name='gallery'),
-      path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
 
 ]
