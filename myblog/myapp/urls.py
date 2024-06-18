4f71c22ea257dca05a20faab8c6fd22d63348bf0
raulradulescu <146931533+raulradulescu@users.noreply.github.com>
2024-06-18 19:11:37 +0300

    commit 369 django gata

--- myblog/myapp/urls.py
+++ myblog/myapp/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('form/', views.form, name='form'),
     path('gallery/', views.gallery, name='gallery'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-
+    path('submit-review/', views.submit_review, name='submit_review'),
+    path('update-review/<int:review_id>/', views.update_review, name='update_review'),
+    path('delete-review/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
--- myblog/myapp/views.py
+++ myblog/myapp/views.py
@@ -1,17 +1,50 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .models import Review
+from .forms import ReviewForm
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'index.html', {'posts': posts})
+    reviews = Review.objects.all().order_by('-date')
+    return render(request, 'index.html', {'posts': posts, 'reviews': reviews})
+
+def index(request):
+    posts = Post.objects.all()
+    reviews = Review.objects.all().order_by('-date')
+    return render(request, 'index.html', {'posts': posts, 'reviews': reviews})
+
+def submit_review(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = ReviewForm()
+    return render(request, 'submit_review.html', {'form': form})
+
+def update_review(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+    else:
+     form = ReviewForm(instance=review)
+    return render(request, 'update_review.html', {'form': form})
+
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    if request.method == 'POST':
+        review.delete_review()
+        return redirect('home')
+    return render(request, 'html/delete_review.html', {'review':review})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'post_detail.html', {'post': post})
-
-def index(request):
-    return render(request, "index.html")
 
 def about(request):
     return render(request, 'about.html')
@@ -26,3 +59,5 @@ def gallery(request):
 
 def chatbot(request):
     return render(request, 'chatbot.html')
+
+
